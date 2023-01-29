29494125b565b182f21672ded1986b8004a22f0a
Dev Gurung <dev.gurung141@gmail.com>
2023-01-29 16:17:10 -0600

    updated version

--- explore.py
+++ explore.py
@@ -7,10 +7,15 @@ import seaborn as sns
 
 
 def plot_variable_pairs(df):
+    '''takes a datafreame and plot graph of pairplots '''
+
     sns.pairplot(data=df.sample(10000),diag_kind='kde', kind='reg',corner = True, plot_kws={'line_kws':{'color':'red'}})
     
 
 def plot_categorical_and_continuous_vars(df, cont_var, cat_var):
+    '''takes a dataframe, a continuous variable, a categorical variable and make 3 graah of categorical
+    variables and a continuous vairable'''
+    
     plt.figure(figsize = (15,5))
     plt.subplot(131)
     sns.boxplot(x=cat_var, y=cont_var, data=df.sample(10000))
